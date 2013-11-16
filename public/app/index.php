02a6de2f98c378289642d9847fd305ef20fb07ca
Fernando de Figueiredo Mendes <fernando.mendes@webca.com.br>
2013-11-16 12:35:42 -0200

    sizeupload

--- public/app/index.php
+++ public/app/index.php
@@ -207,8 +207,11 @@ else {
 						<div id='imageloadstatus' style='display:none'>
 							<img src="loader.gif" alt="Uploading...."/>
 						</div>
-						<div id='imageloadbutton' style="background-image: url('/app/img/button-upload.png'); background-repeat: no-repeat;">
-							<input type="file" name="photoimg" id="photoimg" style="-moz-opacity:0 ; filter:alpha(opacity: 0); opacity: 0; z-index: 2;" />
+						<div id='imageloadbutton' style="background-image: url('/app/img/button-upload.png'); background-repeat: no-repeat;border-radius: 5px;
+width: 123px;
+height: 34px;">
+							<input type="file" name="photoimg" id="photoimg" style="-moz-opacity:0 ; filter:alpha(opacity: 0); opacity: 0; z-index: 2;width: 123px;
+height: 34px;" />
 						</div>
 						</form>
 					</div>
