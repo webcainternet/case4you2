48c6fed6a4de657451f7060c75750868b661a62c
Fernando de Figueiredo Mendes <fernando.mendes@webca.com.br>
2013-11-17 17:24:23 -0200

    aa

--- public/casefouryou/1/index.php
+++ public/casefouryou/1/index.php
@@ -73,40 +73,11 @@ else
 <html>
  
 <head>
-<meta charset="utf-8" />
-
-<style type="text/css">
-@font-face {
-  font-family: "c4y1";
-  src: url(http://capamaniacos.com.br/css/fontes/alrightsans-regularitalic-v3.ttf);
-}
-@font-face {
-  font-family: "c4y2";
-  src: url(http://capamaniacos.com.br/css/fontes/alrightsans-bolditalic-v3.ttf);
-}
-.fontc4y1 { 
-  font-family: "c4y1", Arial, Verdana;
-  font-size: 12px;
-}
-
-.fontc4y2 { 
-  font-family: "c4y2", Arial, Verdana;
-  font-size: 14px;
-  color: #6aa11a;
-}
-body, p {
-font-family: "c4y1", Arial, Verdana;
-}
-
-</style>
-
   <link rel="stylesheet" href="/case4you/0/jquery-ui.css" />
   <script src="/case4you/0/jquery-1.9.1.js"></script>
   <script src="/case4you/0/jquery-ui.js"></script>
   <link rel="stylesheet" href="/case4you/0/style.css" />
- 
-	<link href="css/dropzone.css" type="text/css" rel="stylesheet" />
-	<script src="dropzone.min.js"></script>>
+
 </head>
  
 <body style="margin: 0px; padding: 0px;font-family: Arial, Helvetica, sans-serif; color: #222222;line-height: 1.3;font-size: 12px; ">
