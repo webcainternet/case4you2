2667b0280310ddbe9f10e614d124f5a4633fb35e
Fernando Mendes <fernando.mendes@webca.com.br>
2013-11-18 19:14:59 -0200

    a

--- public/instagram4you/instagram/instagram.config.php
+++ public/instagram4you/instagram/instagram.config.php
@@ -2,7 +2,7 @@
 // Setup class
   $instagram = new Instagram(array(
     'apiKey'      => 'bfe85b5e42344c98b58886cf8cfce096',
-    'apiSecret'   => 'bfe85b5e42344c98b58886cf8cfce096',
+    'apiSecret'   => 'afb94af2809148bcad9c84bea67ec72b',
     'apiCallback' => 'http://case4you.com.br/instagram4you/instagram/success.php' // must point to success.php
   ));
 
