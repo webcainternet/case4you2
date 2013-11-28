4a3befec5bd2375ec37336add7597e9b676b3f6b
Fernando Mendes <fernando.mendes@webca.com.br>
2013-11-28 14:42:47 -0200

    aa

--- public/app/js/montacapa.js
+++ public/app/js/montacapa.js
@@ -20,11 +20,10 @@ function MontaCapa(modelo, layout) {
    $('#divcapinha').prepend(response);
    });
 
-   if (modelo == '0') { document.getElementById("mascarasuperior").style.backgroundImage = "url('/app/img/mask-iphone4-top.png')"; }
-   if (modelo == '1') { document.getElementById("mascarasuperior").style.backgroundImage = "url('/app/img/mask-iphone5-top.png')"; }
-   if (modelo == '2') { document.getElementById("mascarasuperior").style.backgroundImage = "url('/app/img/mask-galaxy3-top.png')"; }
-   if (modelo == '3') { document.getElementById("mascarasuperior").style.backgroundImage = "url('/app/img/mask-galaxy4-top.png')"; }
-
+   if (modelo == '0') { $('#mascarasuperior').css("background-image", "url(/app/img/mask-iphone4-top.png)"); }
+   if (modelo == '1') { $('#mascarasuperior').css("background-image", "url(/app/img/mask-iphone5-top.png)"); }
+   if (modelo == '2') { $('#mascarasuperior').css("background-image", "url(/app/img/mask-galaxy3-top.png)"); }
+   if (modelo == '3') { $('#mascarasuperior').css("background-image", "url(/app/img/mask-galaxy4-top.png)"); }
 
 }
 
