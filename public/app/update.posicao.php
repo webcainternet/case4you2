82ffd8477c65d5139bb61962ac8eb5cabcfc211a
Fernando de Figueiredo Mendes <fernando.mendes@webca.com.br>
2013-11-28 05:26:45 -0200

    update.posicao.php

---
+++ public/app/update.posicao.php
@@ -0,0 +1,43 @@
+<?php
+ini_set("memory_limit","128M");
+
+$idcsession = $_GET["idcsession"];
+
+include '../config.php';
+
+$gidcsession = $idcsession;
+$gmodelo = $_GET["modelo"];
+$glayout = $_GET["layout"];
+$gposicao = $_GET["posicao"];
+$gimagem = $_GET["imagem"];
+$qnheight = $_GET["nheight"];
+$qnwidth = $_GET["nwidth"];
+$qnleft = $_GET["nleft"];
+$qntop = $_GET["ntop"];
+
+$dblink = mysql_connect(DB_HOSTNAME, DB_USERNAME, DB_PASSWORD);
+mysql_select_db(DB_DATABASE,$dblink);
+
+$sql_statement = "UPDATE  `case4you2`.`c4y_capasconstrucao` set 
+`nheight` = ".$qnheight.",
+`nwidth` = ".$qnwidth.",
+`nleft` = ".$qnleft.",
+`ntop= ".$qntop."`
+WHERE 
+`idcsession` = ".$gidcsession." AND
+set `modelo` = ".$gmodelo." AND
+set `layout` = ".$glayout." AND
+set `posicao` = ".$gposicao."
+";
+
+$result = mysql_query($sql_statement,$dblink);
+
+if (!$result) {
+    die('Invalid query: ' . mysql_error());
+}
+else {
+    echo "ok";    
+}
+
+?>
+
