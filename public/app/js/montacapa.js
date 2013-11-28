1fd571429ddf620061c38034b7fbf6e38b260662
Fernando de Figueiredo Mendes <fernando.mendes@webca.com.br>
2013-11-28 02:17:15 -0200

    set

--- public/app/js/montacapa.js
+++ public/app/js/montacapa.js
@@ -25,7 +25,7 @@ function MontaCapa(modelo, layout) {
    if (modelo == '2') { document.getElementById("mascarasuperior").style.backgroundImage = "url('/app/img/mask-galaxy3-top.png')"; }
    if (modelo == '3') { document.getElementById("mascarasuperior").style.backgroundImage = "url('/app/img/mask-galaxy4-top.png')"; }
 
-   alert(document.getElementById("bordapontilhada").height);
+   document.getElementById("bordapontilhada").style.height = '555px';
 
 
 }
