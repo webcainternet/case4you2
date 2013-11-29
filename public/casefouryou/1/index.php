c91ec3959310a47287878953cc3f8c9d32414491
Fernando Mendes <fernando.mendes@webca.com.br>
2013-11-29 18:49:42 -0200

    fe

--- public/casefouryou/1/index.php
+++ public/casefouryou/1/index.php
@@ -5,44 +5,7 @@ $(window).load(function() {
   });
 </script> 
 
-<?php
-//incluindo a classe de conexão com o facebook
-require '../facebook-php-sdk/src/facebook.php';
-
- 
-/*
-* ID da App, vocé obteve isso na ultima
-* página de geração do seu aplicativo no facebook
-*/
-$App_ID = '533870426700085';
-/*
-* App Secret, você obteve isso na ultima
-* página de geração do seu aplicativo no facebook
-*/
-$App_Secret = 'd61ec652232c75af2667e8a2f5c2ba88';
- 
-//Instanciando o Objeto da classe do facebook
-$facebook = new Facebook(array(
-        'appId'  => $App_ID ,
-        'secret' => $App_Secret
-));
- 
-//Pegando Id do usuário Logado
-$o_user = $facebook->getUser();
-
-//echo $o_user; 
-/*
-* Verificando se está conectado
-*/
-if($o_user == 0)
-{
-    //Envia para a página de permissão do facebook, nela voce irá dar permissão ao aplicativo
-    //acessar dados da sua conta
-    $url = $facebook->getLoginUrl(array('scope' => array('user_about_me','user_hometown','user_photos','friends_photos','read_stream','friends_likes','photo_upload','publish_stream','status_update','video_upload')));
-    //header("Location:".$url);
-    //echo "<script language=javascript>location.href ='".$url."'</script>";
-    ?>
-    <style>
+<style>
 
     @font-face {
           font-family: "c4y1";
@@ -86,7 +49,46 @@ if($o_user == 0)
         font-weight: normal;
         margin: 30px;
     }
-    </style>
+</style>
+
+<?php
+//incluindo a classe de conexão com o facebook
+require '../facebook-php-sdk/src/facebook.php';
+
+ 
+/*
+* ID da App, vocé obteve isso na ultima
+* página de geração do seu aplicativo no facebook
+*/
+$App_ID = '533870426700085';
+/*
+* App Secret, você obteve isso na ultima
+* página de geração do seu aplicativo no facebook
+*/
+$App_Secret = 'd61ec652232c75af2667e8a2f5c2ba88';
+ 
+//Instanciando o Objeto da classe do facebook
+$facebook = new Facebook(array(
+        'appId'  => $App_ID ,
+        'secret' => $App_Secret
+));
+ 
+//Pegando Id do usuário Logado
+$o_user = $facebook->getUser();
+
+//echo $o_user; 
+/*
+* Verificando se está conectado
+*/
+if($o_user == 0)
+{
+    //Envia para a página de permissão do facebook, nela voce irá dar permissão ao aplicativo
+    //acessar dados da sua conta
+    $url = $facebook->getLoginUrl(array('scope' => array('user_about_me','user_hometown','user_photos','friends_photos','read_stream','friends_likes','photo_upload','publish_stream','status_update','video_upload')));
+    //header("Location:".$url);
+    //echo "<script language=javascript>location.href ='".$url."'</script>";
+    ?>
+    
     <div style="text-align: center;margin: 30px;">Voc&ecirc; precisa estar logado no facebook para conseguir utilizar suas fotos!</div>
     <?php
     echo "<div style='text-align: right;margin-left: 30px;'><a id=\"clickface\" class=\"botaof button\" href=\"$url\">Entrar com Facebook</a></div>";
@@ -124,7 +126,7 @@ else
         //para ver todos os campos retornados
         $albums = $facebook->api('/me/albums');
         ?>
-            <div>Autentica&ccedil;&atilde;o efetuada!</div>
+            <div style="text-align: center;margin: 30px;">Autentica&ccedil;&atilde;o efetuada. Esta janela pode ser fechada!</div>
         <?php
 
 
