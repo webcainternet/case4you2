51f0aab2fb7395c8ef1e664fd21558baf5fdd331
Fernando Mendes <fernando.mendes@webca.com.br>
2013-11-29 18:53:51 -0200

    aa

--- public/instagram4you/instagram/home.php
+++ public/instagram4you/instagram/home.php
@@ -5,6 +5,52 @@ $(window).load(function() {
   });
 </script> 
 
+<style>
+
+    @font-face {
+          font-family: "c4y1";
+          src: url(/catalog/view/theme/theme254/stylesheet/alrightsans-regularitalic-v3.ttf);
+    }
+    @font-face {
+          font-family: "c4y2";
+          src: url(/catalog/view/theme/theme254/stylesheet/alrightsans-bolditalic-v3.ttf);
+    }
+    .fontc4y1 { 
+          font-family: "c4y1", Arial, Verdana;
+          font-size: 12px;
+    }
+
+    .fontc4y2 { 
+          font-family: "c4y2", Arial, Verdana;
+          font-size: 14px;
+          color: #6aa11a;
+    }
+    body, p {
+        font-family: "c4y1", Arial, Verdana;
+        margin:0px; padding:0px;
+        background-color: transparent;
+    }
+
+    .botaof {
+        display: inline-block;
+        text-decoration: none;
+        cursor: pointer;
+        background: url('/catalog/view/theme/theme254/image/button.png') repeat-x left top;
+        font-size: 12px;
+        line-height: 18px;
+        border-radius: 5px;
+        color: #fff;
+        display: inline-block;
+        padding: 4px 10px;
+        white-space: nowrap;
+        font-size: 12px;
+        line-height: 19px;
+        text-transform: uppercase;
+        font-weight: normal;
+        margin: 30px;
+    }
+</style>
+
 <?php
 session_start();
 if($_GET['id']=='logout')
@@ -27,7 +73,10 @@ else
     //header('Location: /instagram4you/instagram/index.php');
   // Display the login button
   $loginUrl = $instagram->getLoginUrl();
-  echo "<a id=\"clickinsta\" class=\"button\" href=\"$loginUrl\">Entrar com Instagram</a>";
+  ?>
+  <div style="text-align: center;margin: 30px;">Voc&ecirc; precisa estar logado no instagram para conseguir utilizar suas fotos!</div>
+  <?php
+  echo "<div style='text-align: right;margin-left: 30px;'><a id=\"clickinsta\" class=\"button\" href=\"$loginUrl\">Entrar com Instagram</a></div>";
 
   //Script refresh
 }
