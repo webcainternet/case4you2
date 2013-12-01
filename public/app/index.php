67847980b431a7e0daebce7798fa59ddb94a2a99
Fernando de Figueiredo Mendes <fernando.mendes@webca.com.br>
2013-12-01 16:44:17 -0200

    wef

--- public/app/index.php
+++ public/app/index.php
@@ -398,6 +398,7 @@ else {*/
 				* Preencha toda a &aacute;rea pontilhada para imprimir nas laterais da capinha
 			</div>
 
+			<div><iframe src="//www.facebook.com/plugins/like.php?href=https%3A%2F%2Fcase4you.com.br%2F&amp;width&amp;layout=standard&amp;action=like&amp;show_faces=true&amp;share=true&amp;height=80&amp;appId=533870426700085" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:80px;" allowTransparency="true"></iframe></div>
 
 		</div>
 
