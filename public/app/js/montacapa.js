c0f9c9722a1505304666b69d22f890fab09a7610
Fernando de Figueiredo Mendes <fernando.mendes@webca.com.br>
2013-12-01 16:00:58 -0200

    asd

--- public/app/js/montacapa.js
+++ public/app/js/montacapa.js
@@ -583,6 +583,41 @@ function moverdir(dvposicao) {
    }
 }
 
+function virarmais(dvposicao) {
+   var eldvposicao=document.getElementById(dvposicao); 
+
+   var eldvposicaoChildren = eldvposicao.childNodes; 
+   for(var i = 0; i < eldvposicaoChildren.length; i++) 
+   { 
+      if (eldvposicaoChildren.item(i).id != null && 
+      eldvposicaoChildren.item(i).id != "" &&
+      eldvposicaoChildren.item(i).id != "dvcontroles" &&
+      eldvposicaoChildren.item(i).id != "dvcontroles1" &&
+      eldvposicaoChildren.item(i).id != "dvcontroles2" &&
+      eldvposicaoChildren.item(i).id != "dvfechar1" &&
+      eldvposicaoChildren.item(i).id != "dvfechar2" &&
+      eldvposicaoChildren.item(i).id != "dvfechar3" &&
+      eldvposicaoChildren.item(i).id != "dvfechar4" &&
+      eldvposicaoChildren.item(i).id != "dvfechar5" &&
+      eldvposicaoChildren.item(i).id != "dvfechar6" &&
+      eldvposicaoChildren.item(i).id != "dvfechar7" &&
+      eldvposicaoChildren.item(i).id != "dvfechar8" &&
+      eldvposicaoChildren.item(i).id != "dvfechar9" &&
+      eldvposicaoChildren.item(i).id != "dvfechar10" &&
+      eldvposicaoChildren.item(i).id != "dvfechar11" &&
+      eldvposicaoChildren.item(i).id != "dvfechar12" &&
+      eldvposicaoChildren.item(i).id != "dvfechar13" &&
+      eldvposicaoChildren.item(i).id != "dvfechar14" &&
+      eldvposicaoChildren.item(i).id != "dvfechar15" ) {
+
+         var elimagem=document.getElementById(eldvposicaoChildren.item(i).id); 
+         alert($(eldvposicaoChildren.item(i).id).css('rotation'));
+
+      }
+      
+   }
+}
+
 function updateposition(idsession, posicao, nheight, nwidth, nleft, ntop)
 {
     document.getElementById("invfr"+posicao).src="https://case4you.com.br/app/update.posicao.php?idcsession="+idsession+"&posicao="+posicao+"&imagem="+imagemurl+"&nheight="+nheight+"&nwidth="+nwidth+"&nleft="+nleft+"&ntop="+ntop;
