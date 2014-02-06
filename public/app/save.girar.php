6da1092da367ae53260d9b3d0f43a56e9b0e5f94
Fernando Mendes <fernando.mendes@webca.com.br>
2014-02-06 19:16:16 -0200

    girar

--- public/app/save.girar.php
+++ public/app/save.girar.php
@@ -17,7 +17,7 @@
 	if (isset($_GET['idsession']) && isset($_GET['posicao']) && isset($_GET['angulo'])){
 
 		$sql = "INSERT INTO case4you.c4y_capasconstrucao_girar (idsession, posicao, angulo)
-				VALUES($_GET['idsession'], $_GET['posicao'], $_GET['angulo'])";
+				VALUES(".$_GET['idsession'].", ".$_GET['posicao'].", ".$_GET['angulo'].")";
 
 		$result = $mysqli->query($sql);
 
