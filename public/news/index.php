ddf2ea38c417aa46448e029dec879520fdd22839
Fernando Mendes <fernando.mendes@webca.com.br>
2014-02-06 15:40:21 -0200

    news

---
+++ public/news/index.php
@@ -0,0 +1,21 @@
+<?php  
+
+	include ../config.php;
+	echo DB_HOSTNAME;
+	echo DB_USERNAME;
+	echo DB_PASSWORD;
+	echo DB_DATABASE;
+/*
+	$host = 'localhost';
+	$user = 'root';
+	$pass = 'root';
+
+
+	$mysqli = new mysqli($host, $user, $pass, "pelo_bem");
+
+	if (mysqli_connect_errno()) {
+    	printf("Connect failed: %s\n", mysqli_connect_error());
+	exit();
+	}
+*/
+?>
