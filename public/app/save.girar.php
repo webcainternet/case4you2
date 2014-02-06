254ea48373c1840d983cf0d67381ac5a2d7f1eb3
Fernando Mendes <fernando.mendes@webca.com.br>
2014-02-06 19:13:03 -0200

    girar

---
+++ public/app/save.girar.php
@@ -0,0 +1,26 @@
+<?php  
+
+	require "../config.php";
+
+	$host = DB_HOSTNAME;
+	$user = DB_USERNAME;
+	$pass = DB_PASSWORD;
+	$base = DB_DATABASE;
+
+	$mysqli = new mysqli($host, $user, $pass, $base);
+
+	if (mysqli_connect_errno()) {
+    	printf("Connect failed: %s\n", mysqli_connect_error());
+	exit();
+	}
+
+	if (isset($_GET['idsession']) && isset($_GET['posicao']) && isset($_GET['angulo'])){
+
+		$sql = "INSERT INTO case4you.c4y_capasconstrucao_girar (idsession, posicao, angulo)
+				VALUES($_GET['idsession'], $_GET['posicao'], $_GET['angulo'])";
+
+		$result = $mysqli->query($sql);
+
+		echo $sql;
+	}
+?>
